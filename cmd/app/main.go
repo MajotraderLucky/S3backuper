decc039a18b3a5b56152c29e2df6e6038e3f434c
ryazanovs <sergey.ryazanov@sfera.zone>
2024-08-19 05:31:35 +0300

    refactor(main): Refactor file uploading logic into a separate function
    - Extract the file opening and uploading logic from the main function into a new function `uploadFile`.
    - This function takes an uploader instance, file path, bucket name, and key path as parameters, and handles the uploading process.
    - Improve code modularity and reusability by isolating specific functionality, making the main function cleaner and more focused.
    - Simplify error handling and enhance the readability and maintainability of the code.

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -23,23 +23,32 @@ func main() {
 		return
 	}
 
-	// Открываем файл
-	file, err := os.Open(cfg.FilePath)
+	err = uploadFile(uploader, cfg.FilePath, cfg.BucketName, cfg.KeyPath)
 	if err != nil {
-		fmt.Println("Ошибка открытия файла:", err)
+		fmt.Println("Ошибка при загрузке файла:", err)
 		return
+	}
+
+	fmt.Println("Файл успешно загружен")
+}
+
+func uploadFile(uploader *s3manager.Uploader, filePath, bucketName, keyPath string) error {
+	// Открываем файл
+	file, err := os.Open(filePath)
+	if err != nil {
+		return err
 	}
 	defer file.Close()
 
 	// Загрузка файла
 	_, err = uploader.Upload(&s3manager.UploadInput{
-		Bucket: aws.String(cfg.BucketName),
-		Key:    aws.String(cfg.KeyPath),
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(keyPath),
 		Body:   file,
 	})
 	if err != nil {
-		fmt.Println("Ошибка при загрузке файла:", err)
-		return
+		return err
 	}
-	fmt.Println("Файл успешно загружен")
+
+	return nil
 }
